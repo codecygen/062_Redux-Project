2b8ee2d9a0e6983414a1e03322c708c9dc4e4f83
gingercook <vahitaras@gmail.com>
2022-03-18 20:26:27 -0400

    increment and decrement buttons added, dispatch function executed

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,17 +1,31 @@
 // Redux-Manipulating-Data-In-Specific-Component-File
 // This is a hook made by react-redux team
 // There is also a useStore hook
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  // Redux-Manipulating-Data-In-Specific-Component-File
+  // This calls dispatch function which then we can call in the component
+  const dispatch = useDispatch();
+
   // Redux-Manipulating-Data-In-Specific-Component-File
   // useSelector automatically subscribes this to the react store file\
   // It will automatically get the latest state of the hook here.
   // If you remove the component from DOM for any reason, redux store file
   // will automatically unsubscribes this file as well.
   const counter = useSelector(state => state.counter);
+
+  // Redux-Manipulating-Data-In-Specific-Component-File
+  const incrementHandler = () => {
+    dispatch({ type: 'increment' });
+  };
+
+  // Redux-Manipulating-Data-In-Specific-Component-File
+  const decrementHandler = () => {
+    dispatch({ type: 'decrement' });
+  };
 
   const toggleCounterHandler = () => {};
 
@@ -20,6 +34,12 @@ const Counter = () => {
       <h1>Redux Counter</h1>
       {/* Redux-Manipulating-Data-In-Specific-Component-File */}
       <div className={classes.value}>{counter}</div>
+      <div>
+        {/* Redux-Manipulating-Data-In-Specific-Component-File */}
+        <button onClick={incrementHandler}>Increment</button>
+        {/* Redux-Manipulating-Data-In-Specific-Component-File */}
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
